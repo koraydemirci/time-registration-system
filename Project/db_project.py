f7e67b0a89c772ff62c8e34c1e20112e88b90053
Ibrahim <alnajjar.ibrahim@gmail.com>
2025-06-03 22:54:21 +0200

    Add project :

--- Project/db_project.py
+++ Project/db_project.py
@@ -1,4 +1,5 @@
-from db.models import DbProjects
+
+from Project.model import DbProjects
 from Customer.model import Customer
 from Employer.model import Employer
 from sqlalchemy.orm import Session
--- db/models.py
+++ db/models.py
@@ -49,35 +49,35 @@ class DbUser(Base):
 #     )
 
 
-class DbProjects(Base):
-    __tablename__ = "projects"
+# class DbProjects(Base):
+#     __tablename__ = "projects"
 
-    id = Column(Integer, primary_key=True, index=True) 
-    name = Column(String)
-    description = Column(String)
-    start_date = Column(Datetime)
-    end_date = Column(Datetime)
-    budget = Column(Float)
-    status = Column(Enum("active", "inactive", "completed", "on hold", name="project_status"), default="active")
-    hour_rate = Column(Float)
+#     id = Column(Integer, primary_key=True, index=True) 
+#     name = Column(String)
+#     description = Column(String)
+#     start_date = Column(Datetime)
+#     end_date = Column(Datetime)
+#     budget = Column(Float)
+#     status = Column(Enum("active", "inactive", "completed", "on hold", name="project_status"), default="active")
+#     hour_rate = Column(Float)
 
-#    foreign keys
-    customer_id = Column(Integer, ForeignKey("customer.id"))
-    employer_id = Column(Integer, ForeignKey("employer.id"))
+# #    foreign keys
+#     customer_id = Column(Integer, ForeignKey("customer.id"))
+#     employer_id = Column(Integer, ForeignKey("employer.id"))
 
-#    relationships
-    customer = relationship(
-        "Customer",
-        back_populates="projects_as_customer",
-        foreign_keys=[customer_id])
+# #    relationships
+#     customer = relationship(
+#         "Customer",
+#         back_populates="projects_as_customer",
+#         foreign_keys=[customer_id])
  
-    employer = relationship(
-        "Employer",
-        back_populates="projects_as_employer",
-        foreign_keys=[employer_id]
-    )
-    project_assignment = relationship("DbProjectAssigned", back_populates="projects")
-    timeblocks = relationship("DbTimeBlock", back_populates="project")
+#     employer = relationship(
+#         "Employer",
+#         back_populates="projects_as_employer",
+#         foreign_keys=[employer_id]
+#     )
+#     project_assignment = relationship("DbProjectAssigned", back_populates="projects")
+#     timeblocks = relationship("DbTimeBlock", back_populates="project")
 
 
 class DbTimeBlock(Base):
@@ -109,15 +109,15 @@ class DbTimeBlock(Base):
     It contains information about the projects assigned to users.
     It has foreign key relationships with the projects and users tables, allowing us to associate each project assignment with a project and a user.
 '''
-class DbProjectAssigned(Base):
-    __tablename__ = "project_assigned"
+# class DbProjectAssigned(Base):
+#     __tablename__ = "project_assigned"
 
-    id = Column(Integer, primary_key=True, index=True)
-    project_id = Column(Integer, ForeignKey("projects.id"))
-    user_id = Column(Integer, ForeignKey("users.id"))
+#     id = Column(Integer, primary_key=True, index=True)
+#     project_id = Column(Integer, ForeignKey("projects.id"))
+#     user_id = Column(Integer, ForeignKey("users.id"))
 
-    projects = relationship("DbProjects", back_populates="project_assignment")
-    users = relationship("DbUser", back_populates="project_assignment", foreign_keys=[user_id])
+#     projects = relationship("DbProjects", back_populates="project_assignment")
+#     users = relationship("DbUser", back_populates="project_assignment", foreign_keys=[user_id])
 
     
     
