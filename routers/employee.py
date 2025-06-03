12b89a2fc18ff3b3022753d9658bda1b87b66535
Ibrahim <alnajjar.ibrahim@gmail.com>
2025-06-03 22:06:50 +0200

    add crud employee

--- routers/employee.py
+++ routers/employee.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from schemas import EmployeeCreate,EmployeeOut
 from db.database import get_db
-from db import crud_employee
+from Employee.crud_employee import Create_Employee , get_employee 
 
 router = APIRouter(
     prefix="/employee",
@@ -11,15 +11,15 @@ router = APIRouter(
 
 @router.post("/", response_model=EmployeeOut)
 def create(employee: EmployeeCreate, db: Session = Depends(get_db)):
-    return crud_employee.Create_Employee(db, employee)
+    return Create_Employee(db, employee)
 
 @router.get("/", response_model=list[EmployeeOut])
 def read_all(db: Session = Depends(get_db)):
-    return crud_employee.get_employee(db)
+    return get_employee(db)
 
 @router.get("/{employee_id}", response_model=EmployeeOut)
 def read(employee_id: int, db: Session = Depends(get_db)):
-    db_employee = crud_employee.get_employee(db, employee_id)
+    db_employee = get_employee(db, employee_id)
     if not db_employee:
         raise HTTPException(status_code=404, detail="Employee not found")
     return db_employee
